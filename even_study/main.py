bd7fdeb729524e78b23c33dcfdfc812318d627e1
Sharon Ku <z5392310@ad.unsw.edu.au>
2023-04-18 18:02:55 +0800

    first commit

--- even_study/main.py
+++ even_study/main.py
@@ -1,6 +1,56 @@
-from event_study import download, mk_rets, mk_events, mk_cars, test_hypo
+""" main.py
+
+Main module for the event_study package. Will run the event study for a single
+stock.
+"""
+from event_study import download
+from event_study import mk_rets
+from event_study import mk_events
+from event_study import mk_cars
+from event_study import test_hypo
+
 
 def main(tic, update_csv=True):
+    """ Implements the event study for a given stock ticker `tic`.
+
+    Parameters
+    ----------
+    tic : str
+        Ticker
+
+    update_csv : bool
+        If True, data will be downloaded. Defaults to True.
+
+    Notes
+    -----
+    This function will perform the following tasks:
+
+    Step 1: download.get_data(tic)
+      Download stock price and recommendation data for a given `tic` and create
+      the files <prc csv> and <rec csv>
+
+    Step 2: mk_rets.mk_ret_df(tic) --> dataframe
+      For a given `tic`, create stock returns from `<prc csv>` and join the market
+      returns.
+
+    Step 3: mk_events.mk_event_df(tic) --> dataframe
+      Process the recommendations in `<rec csv>` and create a data frame with the
+      events of interest.
+
+    Step 4: mk_cars.mk_cars_df(ret_df, event_df) --> dataframe
+      Creates a dataframe with the CARs for each event of interest, where:
+      `ret_df` : dataframe
+          Output of `mk_rets.mk_ret_df`
+      `event_df` : dataframe
+          Output of `mk_events.mk_event_df`
+
+    Step 5: test_hypo.calc_tstats(cars_df)
+      Hypothesis testing using t-statistics, where
+      `car_df` : dataframe
+        The output of mk_cars.mk_cars_df(ret_df, event_df)
+
+
+    """
     # Step 1: Download stock price and recommendation data for `tic`
     if update_csv is True:
         download.get_data(tic)
@@ -20,7 +70,10 @@ def main(tic, update_csv=True):
     res = test_hypo.calc_tstats(cars_df)
     print(res)
 
+
 if __name__ == "__main__":
     tic = 'TSLA'
     update_csv = False
     main(tic=tic, update_csv=update_csv)
+
+
